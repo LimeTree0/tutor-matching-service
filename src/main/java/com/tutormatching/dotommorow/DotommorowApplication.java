5d0c472fedd9f627a8d2343fddd7ad198b40d6b6
LimeTree0 <bum9516@gmail.com>
2024-01-02 18:26:42 +0900

    MybatisConfig.java 삭제

--- src/main/java/com/tutormatching/dotommorow/DotommorowApplication.java
+++ src/main/java/com/tutormatching/dotommorow/DotommorowApplication.java
@@ -1,11 +1,8 @@
 package com.tutormatching.dotommorow;
 
-import com.tutormatching.dotommorow.config.MybatisConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
-@Import(MybatisConfig.class)
 @SpringBootApplication
 public class DotommorowApplication {
 
