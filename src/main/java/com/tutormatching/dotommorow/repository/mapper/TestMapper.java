d511a0d24d8faa02b1fb785b743709d7fac1019d
LimeTree0 <bum9516@gmail.com>
2024-01-03 15:33:47 +0900

    TestMapper.xml 제거 및 어노테이션을 이용한 쿼리 작성법 추가(TestMapper Interface에 적용되어 있음)

--- src/main/java/com/tutormatching/dotommorow/repository/mapper/TestMapper.java
+++ src/main/java/com/tutormatching/dotommorow/repository/mapper/TestMapper.java
@@ -1,19 +1,24 @@
 package com.tutormatching.dotommorow.repository.mapper;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 @Mapper
 public interface TestMapper {
+    @Insert("INSERT INTO test(name) VALUES(#{name})")
     void save(String name);
 
+    @Update("UPDATE test SET name = #{name} WHERE id = #{id}")
     void update(@Param("id") Long id, @Param("name") String name);
 
+    @Select("SELECT name FROM test")
     List<String> findAll();
 
+    @Select("SELECT name FROM test WHERE id = #{id}")
     String findById(Long name);
-
-
 }
